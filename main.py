5d85d66733f766cdb4b908a1bc2e679df9e23981
Ninnjah <ninnjah.mail@gmail.com>
2021-10-26 00:43:37 +0300

    Add stopwatch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from typing import Tuple
 
 from PyQt5 import QtWidgets, QtCore
@@ -10,6 +11,7 @@ from ui.main_ui import Ui_MainWindow as MainWindow
 class ClockUI(QtWidgets.QMainWindow, MainWindow):
     clock_timer: QTimer
     stopwatch_timer: QTimer
+    stopwatch_time: float
     timer_timer: QTimer
     timer_time: int
     timer_progress_list: tuple
@@ -18,6 +20,7 @@ class ClockUI(QtWidgets.QMainWindow, MainWindow):
         super().__init__(parent)
         self.setupUi(self)
 
+        # Setup UI
         self.timer_progress_list = (
             self.timer_progress,
             self.timer_progress_2,
@@ -27,7 +30,7 @@ class ClockUI(QtWidgets.QMainWindow, MainWindow):
             self.timer_progress_6,
         )
         for i in self.timer_progress_list:
-            i.setValue(0)
+            i.setValue(20)
 
         self.LCD.display("00:00")
         self.LCD_stopwatch.display("00:00:00")
@@ -39,10 +42,39 @@ class ClockUI(QtWidgets.QMainWindow, MainWindow):
         self.clock_timer.timeout.connect(self.update_clock)
         self.clock_timer.start()
 
+        # Set stopwatch timer
+        self.stopwatch_timer = QTimer(self)
+        self.stopwatch_timer.setInterval(1)
+        self.stopwatch_timer.timeout.connect(self.update_stopwatch)
+
+        # Set buttons
+        self.button_stopwatch.clicked.connect(self.start_stopwatch)
+
+    def start_stopwatch(self):
+        if self.button_stopwatch.text() == "Старт":
+            self.button_stopwatch.setText("Стоп")
+            self.stopwatch_time = time.monotonic()
+            self.stopwatch_timer.start()
+        else:
+            self.button_stopwatch.setText("Старт")
+            self.stopwatch_timer.stop()
+
     def update_clock(self):
         current_time = QtCore.QTime.currentTime()
-        str_current_time = current_time.toString('hh:mm')
+        str_current_time = current_time.toString("hh:mm")
         self.LCD.display(str_current_time)
+
+    def update_stopwatch(self):
+        current_time: float = time.monotonic()
+
+        str_current_time = time.strftime(
+            "%M:%S:", time.gmtime(current_time - self.stopwatch_time)
+        )
+        print(current_time)
+        print(int(current_time*100))
+        self.LCD_stopwatch.display(
+            f"{str_current_time}{int(current_time*100)%100:02d}"
+        )
 
 
 if __name__ == "__main__":
